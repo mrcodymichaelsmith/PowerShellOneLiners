db4f181485573e983d443923203d553cf362a371
Cody Michael Smith <70455401+mrcodymichaelsmith@users.noreply.github.com>
2023-05-01 14:00:56 -0400

    Update OneLiners.ps1
    Added quser  and get serialnumber

--- OneLiners.ps1
+++ OneLiners.ps1
@@ -1,5 +1,13 @@
 
+$DaComp = "INSERTCOMPUTERNAMHERE"
+
 
 #Get time since last book
 #Get-Uptime does not work for me???
-(Get-Date) - (Get-CimInstance Win32_OperatingSystem -ComputerName INSERTCOMPUTERNAME).LastBootupTime
+(Get-Date) - (Get-CimInstance Win32_OperatingSystem -ComputerName $DaComp).LastBootupTime
+
+#Who is logged in?
+Invoke-Command -ComputerName $DaComp -ScriptBlock{quser}
+
+#Get Serial Number.
+Invoke-Command -ComputerName  $DaComp -ScriptBlock{WMIC BIOS GET SERIALNUMBER}
