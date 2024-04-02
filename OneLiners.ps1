bf0491de5fe4e5f9891c3f4edd5a2b876b23fdde
Cody Michael Smith <70455401+mrcodymichaelsmith@users.noreply.github.com>
2024-04-02 16:45:29 -0600

    Update OneLiners.ps1
    Trying to make a tool that does all of this at once.

--- OneLiners.ps1
+++ OneLiners.ps1
@@ -1,4 +1,25 @@
 
+Add-WindowsCapability -Name Rsat.ActiveDirectory.DS-LDS.Tools~~~~0.0.1.0 -Online
+# Checks to see if the source computer is online.
+while( $ConfirmComp1 -ne 'y' ){
+    $DaComp = Read-Host -Prompt 'Enter the computer to check'
+
+    if( -not ( Test-Connection -ComputerName $DaComp -Count 2 -Quiet ) ){
+        Write-Warning "$DaComp is not online. Please enter another computer name."
+        continue
+    }
+    Get-ADComputer $DaComp 
+    #$DaDate = (Get-Date) - (Get-CimInstance Win32_OperatingSystem -ComputerName $DaComp).LastBootupTime
+
+    #$DaDate
+    #Who is logged in?
+    Invoke-Command -ComputerName $DaComp -ScriptBlock{
+        quser
+        WMIC BIOS GET SERIALNUMBER
+    }
+}
+
+<#
 $DaComp = "INSERTCOMPUTERNAMHERE"
 
 
@@ -11,3 +32,4 @@ Invoke-Command -ComputerName $DaComp -ScriptBlock{quser}
 
 #Get Serial Number.
 Invoke-Command -ComputerName  $DaComp -ScriptBlock{WMIC BIOS GET SERIALNUMBER}
+#>
